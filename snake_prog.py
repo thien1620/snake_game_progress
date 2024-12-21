053b56bb5b469b7d21fd002ef1eacd1c09c5ce29
thien1620 <thien16201310@gmail.com>
2024-12-21 22:08:55 +0700

    Add files via upload

---
+++ snake_prog.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+# Page Configuration
+st.set_page_config(page_title="Game Update Progress", page_icon="🎮", layout="centered")
+
+st.title('When is the next Snake Game UPDATE?')
+
+st.link_button("In case you don't know what snake game is, click here to play",'https://thien162013.itch.io/snake-game')
+
+with st.expander('Update 1.0'):
+    st.write('Basically everything')
+with st.expander('Update 1.1'):
+    st.write('Gamemode bugfixes (100%)')
+    st.progress(100)
+    st.write('Some Small modifications(100%)')
+    st.progress(100)
+    st.success('Update 1.1 realeased!')
+with st.expander('Update 1.2'):
+    st.write('Achievements system (100%)')
+    st.progress(100)
+    st.write('Pause menu (0%)')
+    st.progress(0)
+    st.write('New gamemode (0%)')
+    st.progress(0)
+    st.info('Note:Taking a break from finishing achievements system. Will come back to developing later.')
